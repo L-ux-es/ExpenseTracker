a3f438ec50970e886721561fc98258a82d8277a2
Leo <leo@>
2025-01-03 16:34:45 -0500

    feat: Create database queries

--- src/main/java/com/luxes/dev/expensetracker/repository/TaskRepository.java
+++ src/main/java/com/luxes/dev/expensetracker/repository/TaskRepository.java
@@ -1,48 +1,57 @@
 package com.luxes.dev.expensetracker.repository;
 
 import com.luxes.dev.expensetracker.model.Task;
-import jakarta.annotation.PostConstruct;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.Assert;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public class TaskRepository {
 
-    private final List<Task> taskList = new ArrayList<>();
+    private final JdbcClient jdbcClient;
+
+    public TaskRepository(JdbcClient jdbcClient) {
+        this.jdbcClient = jdbcClient;
+    }
 
     public List<Task> findAll() {
-        return taskList;
+        return jdbcClient.sql("SELECT * from expense").query(Task.class).list();
     }
 
     public Optional<Task> findById(int id) {
-        return taskList.stream().filter(task -> task.id() == id).findFirst();
+        return jdbcClient.sql("SELECT id,description,category,date_creation,cost FROM expense WHERE id =?").param(id).query(Task.class).optional();
     }
 
+
     public void create(Task task) {
-        taskList.add(task);
+        var created = jdbcClient.sql("INSERT INTO expense VALUES (?,?,?,?,?)")
+                .params(List.of(task.id(), task.description(), task.category(), task.dateCreation(), task.cost())).update();
+        Assert.state(created == 1, "Failed to create task" + task.description());
     }
 
     public void update(Task task, int id) {
-        Optional<Task> taskOptional = findById(id);
-        if (taskOptional.isPresent()) {
-            taskList.set(taskList.indexOf(taskOptional.get()), task);
-        }
+        var updated = jdbcClient.sql("UPDATE expense SET description=?, category=?,date_creation=?,cost=? WHERE id=?")
+                .params(List.of(task.description(), task.category(), task.dateCreation(), task.cost(), id))
+                .update();
+        Assert.state(updated == 1, "Failed to update task" + task.description());
     }
 
     public void delete(int id) {
-        taskList.removeIf(task -> task.id()==id);
+        var deleted = jdbcClient.sql("DELETE FROM expense WHERE id = ?").param(id).update();
+        Assert.state(deleted == 1, "Failed to delete expense with id " + id);
     }
 
-    @PostConstruct
-    private void init() {
-        taskList.add(new Task(1, "Buy a candy", "Groceries", LocalDate.now().minusDays(18), 3.09));
-        taskList.add(new Task(2, "Buy a chocolate", "Groceries", LocalDate.now().minusWeeks(5), 1.58));
-        taskList.add(new Task(3, "Buy a pant", "Clothing", LocalDate.now().minusMonths(1), 15.00));
+      public int count() {
+        return jdbcClient.sql("SELECT count(*) FROM expense").query().listOfRows().size();
     }
+
+    public void saveAll(List<Task> tasks) {
+        tasks.stream().forEach(this::create);
+    }
+
 
 
 }
