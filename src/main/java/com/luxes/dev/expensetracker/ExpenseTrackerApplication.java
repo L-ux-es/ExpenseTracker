f01245e3cfabdf83cd5ad72460a41b45c6742845
Leo <leo@>
2025-01-09 22:43:52 -0500

    refactor: Change property data to environment variables

--- src/main/java/com/luxes/dev/expensetracker/ExpenseTrackerApplication.java
+++ src/main/java/com/luxes/dev/expensetracker/ExpenseTrackerApplication.java
@@ -3,11 +3,30 @@ package com.luxes.dev.expensetracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 @SpringBootApplication
 public class ExpenseTrackerApplication {
 
     public static void main(String[] args) {
+        loadEnvVariables();
         SpringApplication.run(ExpenseTrackerApplication.class, args);
     }
 
+    private static void loadEnvVariables() {
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(".env"));
+            for (String line : lines) {
+                String[] parts = line.split("=", 2);
+                if (parts.length == 2) {
+                    System.setProperty(parts[0], parts[1]);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
